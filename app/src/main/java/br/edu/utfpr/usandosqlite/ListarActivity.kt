e4992a7c019f371015a34397aea29f97ae55737e
mathe <matheusemanuels@hotmail.com>
2024-08-29 19:08:17 -0300

    Adicionando botão de incluir cadastro

--- app/src/main/java/br/edu/utfpr/usandosqlite/ListarActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite/ListarActivity.kt
@@ -1,27 +1,48 @@
+
 package br.edu.utfpr.usandosqlite
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.usandosqlite.adapter.ElementoListaAdapter
 import br.edu.utfpr.usandosqlite.database.DatabaseHandler
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListarActivity : AppCompatActivity() {
 
-    private lateinit var lvRegistros : ListView
-    private lateinit var banco  :DatabaseHandler
+    private lateinit var lvRegistro : ListView
+    private lateinit var btIncluir : FloatingActionButton
+
+    private lateinit var banco : DatabaseHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listar)
 
-        lvRegistros = findViewById( R.id.lvRegistros )
-        banco = DatabaseHandler(this)
+        lvRegistro = findViewById( R.id.lvRegistros )
+        btIncluir = findViewById(R.id.btIncluir )
 
-        val registros = banco.listCursor()
+        btIncluir.setOnClickListener{
+            val intent = Intent( this, MainActivity::class.java )
+            startActivity( intent )
+        }
 
-        val adapter = ElementoListaAdapter(this,registros)
+        banco = DatabaseHandler( this )
 
-        lvRegistros.adapter = adapter
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        val registros = banco.listCursor()
+        val adapter = ElementoListaAdapter( this, registros )
+        lvRegistro.adapter = adapter
+    }
+
 }
